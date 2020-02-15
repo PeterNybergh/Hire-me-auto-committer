b764fa924fce383dae8f6b18a076ce4c514a8825
Peppe <peter.nybergh@aalto.fi>
2020-02-15 16:23:22 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from datetime import timedelta, date
+import os
+import random as rd
+
+
+
+def main(dirs, dates):
+    pass
+
+
+if __name__ == "__main__":
+
+    main({os.getcwd():'https://github.com/PeterNybergh/Hire-me-auto-committer'},
+         )
+
